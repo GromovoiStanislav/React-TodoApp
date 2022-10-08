b775c2fe2e0720ac0698cd02ac803ff2e7ee4e0b
Stanislav <gromila12@rambler.ru>
2022-10-08 11:02:24 +0600

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import { useState } from 'react'
-import TodoForm from './components/Todos/TodoForm'
-import TodoList from './components/Todos/TodoList'
-import './App.css'
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import TodoForm from './components/Todos/TodoForm';
+import TodoList from './components/Todos/TodoList';
+import './App.css';
 
 function App() {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState([]);
 
   const addTodoHandler = (text) => {
-    setTodos([...todos, text])
-  }
+    const newTodo = {
+      text,
+      id: uuidv4(),
+      isCompleted: false,
+    };
+
+    setTodos([...todos, newTodo]);
+  };
 
   const deleteTodoHandler = (id) => {
-    setTodos(todos.filter((_, idx) => idx !== id))
-  }
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
 
   return (
     <div className="App">
@@ -20,7 +27,7 @@ function App() {
       <TodoForm addTodo={addTodoHandler} />
       <TodoList todos={todos} deleteTodo={deleteTodoHandler} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Todos/Todo.js
+++ src/components/Todos/Todo.js
@@ -1,13 +1,13 @@
-import { RiTodoFill } from 'react-icons/ri'
-import styles from './Todo.module.css'
+import { RiTodoFill } from 'react-icons/ri';
+import styles from './Todo.module.css';
 
-function Todo({ todo, index, deleteTodo }) {
+function Todo({ todo, deleteTodo }) {
   return (
-    <div className={styles.todo} onDoubleClick={() => deleteTodo(index)}>
+    <div className={styles.todo} onDoubleClick={() => deleteTodo(todo.id)}>
       <RiTodoFill className={styles.todoIcon} />
-      <div className={styles.todoText}>{todo}</div>
+      <div className={styles.todoText}>{todo.text}</div>
     </div>
-  )
+  );
 }
 
-export default Todo
+export default Todo;
